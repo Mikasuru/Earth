ac7d7b88e652688844a666289d3b31162327bf92
Meow ku <83948007+Mikasuru@users.noreply.github.com>
2022-01-05 20:02:35 +0700

    Create covid.ts

---
+++ dangerous/covid.ts
@@ -0,0 +1,33 @@
+/*
+Source by Frenzy
+Edited by me
+*/
+
+
+var sick = false
+  
+
+
+function life(number: 100) {
+if(sick == false) {
+// Do a normal day.
+console.log("a normal days.");
+} else {
+if(sick == true) {
+const GetDisease = "CoronaVirus"
+if(GetDisease == "CoronaVirus") {
+// We're reducing the oxygen.
+var human_oxygen = 100
+  human_oxygen = 15
+console.log("You get invected by COVID");
+} else {
+console.log("you have a disease. but calm this can be cured.");
+    }
+   }
+  }
+}
+
+sick = true
+if (sick == true) {
+
+}
