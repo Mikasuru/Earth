dbe763ea70342db62907fa33f4ef8197bb4f5ee1
Meow ku <83948007+Mikasuru@users.noreply.github.com>
2022-01-05 19:59:59 +0700

    Create earth.ts

---
+++ earth.ts
@@ -0,0 +1,146 @@
+const Covid = require("./covid.ts")
+console.log("Loaded Covid")
+sleep(3000)
+const Happiness = require("./happiness.ts")
+console.log("Loaded Anti Sadness")
+sleep(3000)
+
+
+console.log("Success.")
+sleep(3000)
+            console.clear()
+  function sleep(ms: number) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < ms);
+}
+
+var core: string = "Generating the earth's core"
+var stone: string = "Generated stone from earth's core"
+var land: string = "Building the earth's land"
+var dirt: string = "Generating the earth's dirt"
+//console.log(`[God-System]: Auto generating trees\nReason: There already have the dirts`) 
+
+let material: string = "Successed Generated materials"
+let ores: string = "Successed generated the ores"
+ //console.log("Located the ores under the land")
+
+var oxygen: string = "Generating The Oxygen"
+var creatures: string = "Generating Cell"
+var water: string = "Oceans appeared"
+var ice: string = "Ice age"
+var volatile: string = "Volatile the ice"
+var evolution: string = "Evolution the Cells"
+//Virus
+
+
+var sick = false
+  
+
+
+function life(number: 100) {
+if(sick == false) {
+// Do a normal day.
+console.log("a normal days.");
+} else {
+if(sick == true) {
+const GetDisease = "CoronaVirus"
+if(GetDisease == "CoronaVirus") {
+// We're reducing the oxygen.
+var human_oxygen = 100
+  human_oxygen = 15
+console.log("You get invected by COVID");
+} else {
+console.log("you have a disease. but calm this can be cured.");
+    }
+   }
+  }
+}
+
+sick = true
+if (sick == true) {
+
+}
+//Earth
+
+console.log(oxygen)
+sleep(3000)
+
+console.log(core)
+  sleep(3000)
+
+console.log(land)
+  sleep(3000)
+
+console.log(dirt)
+  sleep(3000)
+
+console.log("Generated some water")
+sleep(3000)
+
+console.log("Successed.")
+sleep(3000)
+console.clear()
+
+console.log("Starting The Earth")
+sleep(4000)
+
+console.log("The Moon Appeared")
+sleep(2000)
+
+console.clear()
+console.log(material)
+sleep(1500)
+console.log(ores)
+sleep(5000)
+console.clear()
+
+console.log(ice + " started everything freeze.")
+  console.log("Continuing the simulation.(15s Remaining)")
+sleep(10000)
+console.clear()
+
+console.log("5s Remaining")
+sleep(5000)
+console.clear()
+
+console.log(volatile + "(10s Remaining)")
+sleep(5000)
+console.clear()
+
+console.log("5s Remaining")
+sleep(5000)
+console.clear()
+
+console.log(water)
+sleep(2000)
+
+console.log(evolution)
+sleep(5000)
+
+console.log("5,000 Years Past")
+sleep(3000)
+console.clear()
+
+console.log("5,000 Years past so many things appears")
+console.log(`====[ Appear List ]====\n- Animals\n- Jungles\n- Much more\n- All cells in the water start evolution\n(10s Remaining)`)
+sleep(10000)
+console.log("10,000 Years Past")
+sleep(3000)
+console.clear()
+
+console.log("Start Evolution the most Monkeys")
+sleep(2500)
+console.log("Success.")
+sleep(1000)
+console.clear()
+//Web here
+console.log("Stone age has started!")
+console.log("Adding IQ to these stone age human")
+sleep(3000)
+console.log("\n[Error]: Unsucess\n[Info]: Reason: Some of their brain cant accept the IQ so they start dying but some of them can accept the IQ\n[Fix]: to fix you need to wait more 30Years to add the IQ agin.")
+sleep(5000)
+console.clear()
+console.log("Thanks for wasted your time with my repo\ni will update more as possible")
