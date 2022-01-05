9454bb38e5ea7d84f7267f3206f842215b6ded82
Meow ku <83948007+Mikasuru@users.noreply.github.com>
2022-01-05 20:00:54 +0700

    Create happiness.ts

---
+++ activities/happiness.ts
@@ -0,0 +1,33 @@
+var happy = 100
+var smile = false
+var happiness = false
+var life1:string = "good life"
+var life2:string = "bad life"
+var sickness = false 
+var virus = false //covid
+  if (happy > 70) {
+  smile = true
+} 
+if (happy < 69) {
+  smile = true
+}
+if (happy < 45) {
+  smile = false
+  happiness = false
+  console.log(life2) 
+  sickness = true
+    if (sickness = true) {
+
+   sickness = false
+   virus = false
+   happy = 100
+   console.log(life1)
+   smile = true
+  
+    }
+}
+
+/*
+Anti Sadness,SiSickness and badlife
+This source make people have a good life and happy and no sickness
+*/
